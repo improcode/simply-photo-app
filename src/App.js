8f74b24e49dc0f2bda07946f6ebdc40131e33440
improcode <art.milkowski@gmail.com>
2018-06-01 19:00:39 +0200

    webcam install

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Webcam, {getScreenshot} from 'react-webcam';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+const cameraStyle = {
+    transform: 'scaleX(-1)',
+};
+const cameraScreenDiv = {
+    margin: 30,
+}
+
+class App extends React.Component {
+    setRef = (webcam) => {
+        this.webcam = webcam;
+    }
+
+    capture = () => {
+        const imageSrc = this.webcam.getScreenshot();
+    };
+
+    render() {
+        return (
+            <div style={cameraScreenDiv}>
+                <Webcam
+                    style={cameraStyle}
+                    audio={false}
+                    height={350}
+                    ref={this.setRef}
+                    screenshotFormat="image/jpeg"
+                    width={350}
+                />
+                <button onClick={this.capture}>Capture photo</button>
+
+
+            </div>
+
+
+
+        );
+    }
 }
 
 export default App;
