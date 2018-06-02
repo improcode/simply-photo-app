4c139debc8f37e35e9e029050d595c6015bb921a
improcode <art.milkowski@gmail.com>
2018-06-02 14:39:29 +0200

    set screenshot to state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Webcam, {getScreenshot} from 'react-webcam';
+import Webcam from 'react-webcam';
 
 const cameraStyle = {
     transform: 'scaleX(-1)',
@@ -11,13 +11,29 @@ const cameraScreenDiv = {
 }
 
 class App extends React.Component {
+
+    constructor() {
+        super()
+
+        console.log('Counter component is created!')
+
+        this.state = {
+            screenshot: ''
+        }
+
+        this.capture = () => this.setState({screenshot: this.webcam.getScreenshot()})
+
+        console.log(this.state)
+    }
+
     setRef = (webcam) => {
         this.webcam = webcam;
     }
 
-    capture = () => {
-        const imageSrc = this.webcam.getScreenshot();
-    };
+    // capture = () => {
+    //     const imageSrc = this.webcam.getScreenshot();
+    //
+    // };
 
     render() {
         return (
@@ -31,10 +47,9 @@ class App extends React.Component {
                     width={350}
                 />
                 <button onClick={this.capture}>Capture photo</button>
-
+                <div><img src={this.state.screenshot} alt=""/></div>
 
             </div>
-
 
 
         );
